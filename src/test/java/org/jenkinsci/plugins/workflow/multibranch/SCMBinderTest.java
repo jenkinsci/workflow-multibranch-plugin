380b8d743a755a059b67f42fc4c5b9f1bb728914
Jesse Glick <jglick@cloudbees.com>
2016-06-02 11:07:04 -0400

    Add some useful things from SCMBinderTest to the test JAR.

--- src/test/java/org/jenkinsci/plugins/workflow/multibranch/SCMBinderTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/multibranch/SCMBinderTest.java
@@ -102,7 +102,7 @@ public class SCMBinderTest {
         WorkflowMultiBranchProjectTest.showIndexing(mp);
     }
 
-    static void assertRevisionAction(WorkflowRun build) {
+    public static void assertRevisionAction(WorkflowRun build) {
         SCMRevisionAction revisionAction = build.getAction(SCMRevisionAction.class);
         assertNotNull(revisionAction);
         SCMRevision revision = revisionAction.getRevision();
@@ -256,7 +256,7 @@ public class SCMBinderTest {
         r.assertLogContains("subsequent content", b);
         r.assertLogContains("not trusting", b);
     }
-    static class WarySource extends GitSCMSource {
+    public static class WarySource extends GitSCMSource {
         WarySource(String id, String remote, String credentialsId, String includes, String excludes, boolean ignoreOnPushNotifications) {
             super(id, remote, credentialsId, includes, excludes, ignoreOnPushNotifications);
         }
