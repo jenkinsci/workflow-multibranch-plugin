7c908dc9409eda22ead2885af3616f435b13ec27
Jesse Glick <jglick@cloudbees.com>
2016-09-17 00:25:16 -0700

    [JENKINS-34564] Adjusted integration test to match modified behavior.

--- src/test/java/org/jenkinsci/plugins/workflow/multibranch/WorkflowBranchProjectFactoryTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/multibranch/WorkflowBranchProjectFactoryTest.java
@@ -25,9 +25,7 @@
 package org.jenkinsci.plugins.workflow.multibranch;
 
 import java.io.File;
-import jenkins.branch.BranchProperty;
 import jenkins.branch.BranchSource;
-import jenkins.branch.DefaultBranchPropertyStrategy;
 import jenkins.plugins.git.GitSCMSource;
 import jenkins.plugins.git.GitSampleRepoRule;
 import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval;
@@ -66,14 +64,14 @@ public class WorkflowBranchProjectFactoryTest {
                 sampleRepo.git("add", "Jenkinsfile");
                 sampleRepo.git("commit", "--all", "--message=flow");
                 WorkflowMultiBranchProject mp = story.j.jenkins.createProject(WorkflowMultiBranchProject.class, "p");
-                mp.getSourcesList().add(new BranchSource(new GitSCMSource(null, sampleRepo.toString(), "", "*", "", false), new DefaultBranchPropertyStrategy(new BranchProperty[0])));
+                mp.getSourcesList().add(new BranchSource(new GitSCMSource(null, sampleRepo.toString(), "", "*", "", false)));
                 WorkflowJob p = scheduleAndFindBranchProject(mp, "dev%2Fmain");
                 assertEquals(1, mp.getItems().size());
                 story.j.waitUntilNoActivity();
                 WorkflowRun b1 = p.getLastBuild();
                 assertEquals(1, b1.getNumber());
                 story.j.assertLogContains("branch=dev/main", b1);
-                story.j.assertLogContains("workspace=dev%2Fmain", b1);
+                story.j.assertLogContains("workspace=dev_main-yVp7JkdR.HpV0KPdCvrFv.9HoUgEAQ2OvxCAZF4W_iQ", b1);
                 verifyProject(p);
                 sampleRepo.write("Jenkinsfile", script.replace("branch=", "Branch="));
             }
@@ -87,7 +85,7 @@ public class WorkflowBranchProjectFactoryTest {
                 WorkflowRun b2 = p.getLastBuild();
                 assertEquals(2, b2.getNumber());
                 story.j.assertLogContains("Branch=dev/main", b2);
-                story.j.assertLogContains("workspace=dev%2Fmain", b2);
+                story.j.assertLogContains("workspace=dev_main-yVp7JkdR.HpV0KPdCvrFv.9HoUgEAQ2OvxCAZF4W_iQ", b2);
                 verifyProject(p);
             }
         });
