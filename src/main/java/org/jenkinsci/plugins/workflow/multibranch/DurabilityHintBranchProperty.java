134c167a5219d8fa3e8fffbbd421b93de726212e
Sam Van Oort <samvanoort@gmail.com>
2017-12-07 20:15:44 -0500

    oops

--- src/main/java/org/jenkinsci/plugins/workflow/multibranch/DurabilityHintBranchProperty.java
+++ src/main/java/org/jenkinsci/plugins/workflow/multibranch/DurabilityHintBranchProperty.java
@@ -37,7 +37,7 @@ public class DurabilityHintBranchProperty extends BranchProperty {
         return durabilityHint;
     }
 
-    DataBoundConstructor
+    @DataBoundConstructor
     public DurabilityHintBranchProperty(@Nonnull FlowDurabilityHint durabilityHint) {
         this.durabilityHint = durabilityHint;
     }
