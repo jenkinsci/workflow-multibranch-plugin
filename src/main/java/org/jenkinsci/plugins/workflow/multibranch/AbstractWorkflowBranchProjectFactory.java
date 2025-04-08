6b0a5713433a4d1790cabfb26fa0d274c8f7cf55
Basil Crow <me@basilcrow.com>
2025-04-08 11:42:02 -0700

    Avoid need for `idealNames` workaround

--- src/main/java/org/jenkinsci/plugins/workflow/multibranch/AbstractWorkflowBranchProjectFactory.java
+++ src/main/java/org/jenkinsci/plugins/workflow/multibranch/AbstractWorkflowBranchProjectFactory.java
@@ -25,6 +25,7 @@
 package org.jenkinsci.plugins.workflow.multibranch;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.BulkChange;
 import hudson.model.Item;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -63,14 +64,19 @@ public abstract class AbstractWorkflowBranchProjectFactory extends BranchProject
 
     @NonNull
     @Override public WorkflowJob setBranch(@NonNull WorkflowJob project, @NonNull Branch branch) {
-        project.setDefinition(createDefinition());
-        BranchJobProperty property = project.getProperty(BranchJobProperty.class);
         try {
-            if (property == null) {
-                project.addProperty(new BranchJobProperty(branch));
-            } else { // TODO may add equality check if https://github.com/jenkinsci/branch-api-plugin/pull/36 or equivalent is implemented
-                property.setBranch(branch);
-                project.save();
+            BulkChange bc = new BulkChange(project);
+            try {
+                project.setDefinition(createDefinition());
+                BranchJobProperty property = project.getProperty(BranchJobProperty.class);
+                if (property == null) {
+                    project.addProperty(new BranchJobProperty(branch));
+                } else { // TODO may add equality check if https://github.com/jenkinsci/branch-api-plugin/pull/36 or equivalent is implemented
+                    property.setBranch(branch);
+                    project.save();
+                }
+            } finally {
+                bc.commit();
             }
         } catch (IOException x) {
             LOGGER.log(Level.WARNING, null, x);
